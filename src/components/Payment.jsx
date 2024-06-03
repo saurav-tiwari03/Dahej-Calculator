a37d2c8ce3d9e45883761efc254ea54cfced7518
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-06-04 03:15:57 +0530

    changes

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -3,7 +3,7 @@ export const Payment = () => {
   return (
     <div className='flex flex-col'>
       <img src={Qr} className='w-[150px] h-[130px]'/>
-      <p className='text-sm'>Or haa Payment ke baad <br></br>Mail krdio iss pr Minimum 10Rs</p>
+      <p className='text-sm'> (Minimum ₹10) <br />Payment ke baad Mail krdio</p>
       <a href="mailto:sauravtiwari5625@gmail.com" className='bg-white py-1 rounded-2xl m-auto px-6'>Mail</a>
     </div>
   )
